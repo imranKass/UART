cc2ede4165e05fee41fbd8f84cdbc707666bce2a
imranKass <121225124+imranKass@users.noreply.github.com>
2025-09-30 22:58:23 -0700

    Update GPIO.h

--- GPIO.h
+++ GPIO.h
@@ -12,7 +12,7 @@
  * To verify the pinout of the user LED, refer to the Tiva C Series TM4C123G LaunchPad User's Guide
  * Link: https://www.ti.com/lit/pdf/spmu296
  *
- * @author Aaron Nanas
+ * 
  */
 
 #include "TM4C123GH6PM.h"
